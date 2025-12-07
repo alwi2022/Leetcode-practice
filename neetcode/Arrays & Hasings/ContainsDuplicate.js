62e5d5dcb5ec1d4e97e0705889c0bcba4bf2f513
Imam Bahri Alwi <115518832+alwi2022@users.noreply.github.com>
2025-12-07 16:14:35 +0700

    practice day 1 : ./ContainsDuplicate.js

---
+++ neetcode/Arrays & Hasings/ContainsDuplicate.js
@@ -0,0 +1,29 @@
+/**  Contains Duplicate
+Solved 
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false
+*/
+
+function hasDuplicateNums(nums) {
+  nums.sort((a, b) => a - b);
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(hasDuplicateNums([1, 2, 3, 3]));
